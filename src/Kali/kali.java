7dd5e101ed9bf040c545d749753c42222498c18b
rahmanarif47 <media.hanan46@gmail.com>
2019-11-22 21:30:24 +0700

    Update kali.java

--- src/Kali/kali.java
+++ src/Kali/kali.java
@@ -5,6 +5,6 @@ public class kali {
     public static void main(String[] args){
         for (int g=0; g<'a'; ++g)
             System.out.printf(0!=g%011?"%5d":"%n%5d",(1+g/9)*(1+g%9));
-        //kondisi if 0!=i%011, kalau true cetak "%5d", kalau false cetak "%n%5d".
+       
     }
 }
